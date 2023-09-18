c6c8e42151a9be5d6c95e567d050388d3a71bb72
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-09-18 09:47:30 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { FavComponent } from './components/fav/fav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'main', component: MainComponent},
+  {path: 'fav', component: FavComponent},
+  {path: '', redirectTo:'main', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
